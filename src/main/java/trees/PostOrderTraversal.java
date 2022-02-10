c8832f28ce481ec39c9502ae1610788d0f575917
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-02-10 10:15:23 +0300

    Post-order traversal

---
+++ src/main/java/trees/PostOrderTraversal.java
@@ -0,0 +1,26 @@
+package trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 145. Binary Tree Postorder Traversal.
+ *
+ * @author Albina Gimaletdinova on 10/02/2022
+ */
+class PostOrderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    private void postorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left, res);
+        postorder(root.right, res);
+        res.add(root.val);
+    }
+}
---
+++ src/main/java/trees/TreeNode.java
@@ -0,0 +1,23 @@
+package trees;
+
+/**
+ * @author Albina Gimaletdinova on 10/02/2022
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
