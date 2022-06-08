d93fdc6635312203abdea9722bcfe75a5092d4d1
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-06-08 10:10:10 +0300

    Delete node in a BST second approach

--- src/main/java/trees/DeleteNode.java
+++ src/main/java/trees/DeleteNode.java
@@ -6,6 +6,7 @@ package trees;
  * @author Albina Gimaletdinova on 08/06/2022
  */
 public class DeleteNode {
+    // solution 1 with replacing node value
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) {
             return null;
@@ -29,6 +30,24 @@ public class DeleteNode {
     private TreeNode findMin(TreeNode root) {
         while (root != null && root.left != null) {
             root = root.left;
+        }
+        return root;
+    }
+
+    // solution 2 - find min in right subtree and connect it with the left subtree
+    public TreeNode deleteNode2(TreeNode root, int key) {
+        if (root == null) return root;
+        if (root.val < key)
+            root.right = deleteNode2(root.right, key);
+        else if (root.val > key)
+            root.left = deleteNode2(root.left, key);
+        else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+
+            TreeNode minRight = findMin(root.right);
+            minRight.left = root.left;
+            root = root.right;
         }
         return root;
     }
