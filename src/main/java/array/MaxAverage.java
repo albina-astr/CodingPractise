304719ffd223fb3d8836b22d95e2aed1ae016da3
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-07-28 12:55:13 +0300

    Find max average another approach

--- src/main/java/array/MaxAverage.java
+++ src/main/java/array/MaxAverage.java
@@ -38,4 +38,20 @@ public class MaxAverage {
         }
         return (double) res / k;
     }
+
+    // solution 3 with CUMULATIVE SUM
+    public double findMaxAverage3(int[] nums, int k) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            sum[i] = sum[i - 1] + nums[i];
+        double res = sum[k - 1] * 1.0 / k;
+        for (int i = k; i < nums.length; i++) {
+            res = Math.max(res, (sum[i] - sum[i - k]) * 1.0 / k);
+        }
+        return res;
+    }
+
+    // but this approach requires MORE space - O(n), while
+    // prev solution requires O(1) space
 }
