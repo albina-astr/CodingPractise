f59aee0eda96a4548cb7ba807085cfabe3e00cf7
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-08-15 11:48:23 +0300

    House robber

--- src/main/java/dp/HouseRobber.java
+++ src/main/java/dp/HouseRobber.java
@@ -1,5 +1,8 @@
 package dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/house-robber/
  *
@@ -22,5 +25,28 @@ public class HouseRobber {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
         return dp[nums.length - 1];
+    }
+
+    //top-down
+    private Map<Integer, Integer> memo = new HashMap<>();
+
+    public int rob2(int[] nums) {
+        return rob(nums, nums.length - 1);
+    }
+
+    private int rob(int[] nums, int houseIndex) {
+        if (houseIndex == 0) {
+            return nums[0];
+        }
+        if (houseIndex == 1) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        if (!memo.containsKey(houseIndex)) {
+            memo.put(houseIndex,
+                    Math.max(rob(nums, houseIndex - 1),
+                            rob(nums, houseIndex - 2) + nums[houseIndex]));
+        }
+        return memo.get(houseIndex);
     }
 }
