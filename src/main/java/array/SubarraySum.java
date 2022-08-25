53ed6a61163e1bbae2d25e23ab93256f78e8ce0b
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-08-25 11:48:23 +0300

    Subarray sum

--- src/main/java/array/SubarraySum.java
+++ src/main/java/array/SubarraySum.java
@@ -1,11 +1,15 @@
 package array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/subarray-sum-equals-k/
  *
  * @author Albina Gimaletdinova on 25/08/2022
  */
 public class SubarraySum {
+    // solution 1
     public int subarraySum(int[] nums, int k) {
         if (nums == null || nums.length == 0) return 0;
 
@@ -18,6 +22,23 @@ public class SubarraySum {
             }
         }
 
+        return res;
+    }
+
+    // solution 2 - Cumulative sum
+    public int subarraySum2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int res = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                res += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
         return res;
     }
 }
