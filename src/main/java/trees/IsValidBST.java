ea61dfd883e0c3eea96ef97642d343ed60b3f126
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-09-27 19:30:12 +0300

    Is valid BST

--- src/main/java/trees/IsValidBST.java
+++ src/main/java/trees/IsValidBST.java
@@ -1,5 +1,8 @@
 package trees;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * https://leetcode.com/problems/validate-binary-search-tree/
  *
@@ -16,5 +19,45 @@ public class IsValidBST {
 
         return isBst(root.left, from, root.val) &&
                 isBst(root.right, root.val, to);
+    }
+
+    // SOLUTION 2
+     public boolean isValidBST2(TreeNode root) {
+         if (root == null) {
+             return true;
+         }
+
+         Deque<NodeData> q = new ArrayDeque<>();
+         q.add(new NodeData(root, Long.MIN_VALUE, Long.MAX_VALUE));
+         while (!q.isEmpty()) {
+             NodeData data = q.poll();
+             if (!validate(data)) return false;
+             if (data.node.left != null) {
+                 q.add(new NodeData(data.node.left, data.from, data.node.val));
+             }
+             if (data.node.right != null) {
+                 q.add(new NodeData(data.node.right, data.node.val, data.to));
+             }
+         }
+         return true;
+     }
+
+     private boolean validate(NodeData data) {
+         TreeNode node = data.node;
+         long from = data.from;
+         long to = data.to;
+         return node.val > from && node.val < to;
+     }
+}
+
+class NodeData {
+    TreeNode node;
+    long from;
+    long to;
+
+    public NodeData(TreeNode node, long from, long to) {
+        this.node = node;
+        this.from = from;
+        this.to = to;
     }
 }
