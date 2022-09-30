5233aa89f14fce95e3b896bfe66286d4141f2d83
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-09-30 19:30:12 +0300

    Same tree

--- src/main/java/trees/SameTree.java
+++ src/main/java/trees/SameTree.java
@@ -53,4 +53,15 @@ public class SameTree {
         }
         return p.val == q.val;
     }
+
+    // recursion
+    public boolean isSameTree2(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+
+        return isSameTree2(p.left, q.left) && isSameTree2(p.right, q.right);
+    }
 }
