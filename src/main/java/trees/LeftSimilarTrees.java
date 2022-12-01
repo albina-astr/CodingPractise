279c8f354ebdd069343f74d96ec1f014011a4119
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2022-12-01 10:10:10 +0300

    Left similar trees

---
+++ src/main/java/trees/LeftSimilarTrees.java
@@ -0,0 +1,29 @@
+package trees;
+
+/**
+ * https://leetcode.com/problems/leaf-similar-trees/
+ *
+ * @author Albina Gimaletdinova on 01/12/2022
+ */
+public class LeftSimilarTrees {
+    private List<Integer> leaf1 = new ArrayList<>();
+    private List<Integer> leaf2 = new ArrayList<>();
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        inorder(root1, leaf1);
+        inorder(root2, leaf2);
+        return leaf1.equals(leaf2);
+    }
+
+    private void inorder(TreeNode root, List<Integer> leafs) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left, leafs);
+        if (root.left == null && root.right == null) {
+            leafs.add(root.val);
+        }
+        inorder(root.right, leafs);
+    }
+}
